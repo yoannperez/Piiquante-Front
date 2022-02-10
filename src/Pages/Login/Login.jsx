b0ff1f9b98f7f3626d62294dd6a23b040447994e
Yoann Perez <yoann.perez@gmail.com>
2022-02-10 20:34:56 +0100

    Login signup done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
@@ -7,16 +7,40 @@ const Login = () => {
         handleSubmit,
         formState: { errors },
     } = useForm();
-    const onSubmit = (data) => console.log(data);
-    console.log(errors);
-    return (
-        <div className='logContainer'>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
-                <input type="password" placeholder="Password" {...register("Password", { required: true })} />
 
-                <input type="submit" value="Se connecter"/>
+    console.log(errors);
+
+    const login = async (data, e) => {
+        e.preventDefault();
+        await fetch("http://localhost:3000/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then(async (res) => {
+                const resData = await res;
+                console.log(resData);
+                if (resData.status === "success") {
+                    //   setMailAdress(getValues('email'));
+                    // alert("Message Sent");
+                } else if (resData.status === "fail") {
+                    console.log("Message failed to send");
+                    // alert("Message failed to send");
+                }
+            });
+    };
+
+    return (
+        <div className="logContainer">
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit(login)}>
+                <input type="text" placeholder="Email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+                <input type="password" placeholder="Password" {...register("password", { required: true })} />
+
+                <input type="submit" value="Se connecter" />
             </form>
         </div>
     );
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -9,13 +9,36 @@ const Signup = () => {
     } = useForm();
     const onSubmit = (data) => console.log(data);
     console.log(errors);
+
+    const signup = async (data, e) => {
+        e.preventDefault();
+        await fetch("http://localhost:3000/api/auth/signup", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then(async (res) => {
+                const resData = await res;
+                console.log(resData);
+                if (resData.status === "success") {
+                    //   setMailAdress(getValues('email'));
+                    // alert("Message Sent");
+                } else if (resData.status === "fail") {
+                    console.log("Message failed to send");
+                    // alert("Message failed to send");
+                }
+            });
+    };
     
     return (
         <div className='logContainer'>
             <h1>SignUp</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
-                <input type="password" placeholder="Password" {...register("Password", { required: true })} />
+            <form onSubmit={handleSubmit(signup)}>
+                <input type="text" placeholder="Email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+                <input type="password" placeholder="Password" {...register("password", { required: true })} />
 
                 <input type="submit" value="Créer un compte"/>
             </form>
