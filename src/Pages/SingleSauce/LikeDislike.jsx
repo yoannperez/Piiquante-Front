5959fbd0bed7c7792835c86090a0de9a833299f2
Yoann Perez <yoann.perez@gmail.com>
2022-02-14 13:10:37 +0100

    Introduce hook useFetch in likeDisklike

--- src/Pages/SingleSauce/LikeDislike.jsx
+++ src/Pages/SingleSauce/LikeDislike.jsx
@@ -2,95 +2,68 @@ import React, { useEffect, useState, useRef } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../utils/context/context";
 import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
+import { useFetch } from "../../utils/hooks/index";
 
 const LikeDislike = (id) => {
     const { user } = useContext(UserContext);
-    const [datas, setDatas] = useState();
+    // const [datas, setDatas] = useState();
 
     const [like, setLike] = useState(0);
 
     const [spinner, setSpinner] = useState(false);
-    const [refresh, setRefresh] = useState(false);
+    // const [refresh, setRefresh] = useState(false);
     const [sendApi, setSendApi] = useState(false);
+
+    const { data, isLoading, error } = useFetch(process.env.REACT_APP_API_ADRESS + "/api/sauces/" + id.id);
+
+    useEffect(() => {
+        if (Object.keys(data).length != 0) {
+        function userId(userid) {
+            return userid === user.userId;
+        }
+        if (data.usersLiked.find(userId)) {
+            setLike(1);
+        } else if (data.usersDisliked.find(userId)) {
+            setLike(-1);
+        } else {
+            setLike(0);
+        }
+    }
+    }, [data]);
+
+    console.log("like : ", like);
 
     const firstUpdate = useRef(true);
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "BEARER " + user.token);
+    // useEffect(() => {
+    //     const EffectHeader = myHeaders;
 
-    useEffect(() => {
-        setSpinner(true);
-        if (user && id) {
-            const EffectHeader = myHeaders;
+    //     if (firstUpdate.current) {
+    //         firstUpdate.current = false;
+    //         return;
+    //     }
 
-
-
-            function userId(userid) {
-                // Function for finding in Array
-                return userid === user.userId;
-            }
-            async function fetchData() {
-                try {
-                    const response = await fetch(process.env.REACT_APP_API_ADRESS + "/api/sauces/" + id.id, {
-                        method: "GET",
-                        headers: EffectHeader,
-                      
-                    });
-                    const data = await response.json();
-                    setDatas(data);
-                    if (data.usersLiked.find(userId)) {
-                        setLike(1);
-                    } else if (data.usersDisliked.find(userId)) {
-                        setLike(-1);
-                    } else {
-                        setLike(0);
-                    }
-                    
-                } catch (err) {
-                    console.log(err);
-                    // setError(true);
-                } finally {
-                    setSpinner(false);
-                }
-            }
-            fetchData();
-        }
-        return () => {
-            setSpinner(false);
-          };
-          // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [id.id, refresh]);
-
-    useEffect(() => {
-        const EffectHeader = myHeaders;
-
-        if (firstUpdate.current) {
-            firstUpdate.current = false;
-            return;
-        }
-
-        setSpinner(true);
-        let toSend = {
-            userId: user.userId,
-            like: like,
-        };
-        async function fetchLike() {
-            try {
-                await fetch(process.env.REACT_APP_API_ADRESS + "/api/sauces/" + id.id + "/like", {
-                    method: "POST",
-                    headers: EffectHeader,
-                    body: JSON.stringify(toSend),
-                });
-            } catch (err) {
-                console.log(err);
-                // setError(true);
-            } finally {
-                setRefresh(!refresh);
-            }
-        }
-        fetchLike();
-    }, [sendApi]);
+    //     setSpinner(true);
+    //     let toSend = {
+    //         userId: user.userId,
+    //         like: like,
+    //     };
+    //     async function fetchLike() {
+    //         try {
+    //             await fetch(process.env.REACT_APP_API_ADRESS + "/api/sauces/" + id.id + "/like", {
+    //                 method: "POST",
+    //                 headers: EffectHeader,
+    //                 body: JSON.stringify(toSend),
+    //             });
+    //         } catch (err) {
+    //             console.log(err);
+    //             // setError(true);
+    //         } finally {
+    //             setRefresh(!refresh);
+    //         }
+    //     }
+    //     fetchLike();
+    // }, [sendApi]);
 
     function handleLike(e) {
         e.preventDefault();
@@ -113,23 +86,27 @@ const LikeDislike = (id) => {
         setSendApi(!sendApi);
     }
 
-    if (datas) {
+    if (error) {
+        return <span>Il y a un problème</span>;
+    }
+
+    if (data) {
         return (
             <>
-                {spinner ? (
+                {isLoading ? (
                     <div className="Loader" style={{ marginTop: "0" }}></div>
                 ) : (
                     <div>
-                        <button  className={`likeComponent + ${(like === 1)? "like" : ""}`} onClick={(e) => handleLike(e)} disabled={like === -1}>
-                            <AiOutlineLike size={30}/>
+                        <button className={`likeComponent + ${like === 1 ? "like" : ""}`} onClick={(e) => handleLike(e)} disabled={like === -1}>
+                            <AiOutlineLike size={30} />
                         </button>
-                        <span>{datas.likes}</span>
+                        <span>{data.likes}</span>
 
-                        <button className={`likeComponent + ${(like === -1) ? "disLike" : ""}`} onClick={(e) => handleDisLike(e)} disabled={like === 1}>
-                            <AiOutlineDislike size={30}/>
+                        <button className={`likeComponent + ${like === -1 ? "disLike" : ""}`} onClick={(e) => handleDisLike(e)} disabled={like === 1}>
+                            <AiOutlineDislike size={30} />
                         </button>
 
-                        {datas.dislikes}
+                        {data.dislikes}
                     </div>
                 )}
             </>
